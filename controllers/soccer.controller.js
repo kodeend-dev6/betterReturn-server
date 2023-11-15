ebd84fe315f82d859c5fec63a49d6c816a80c83a
kodeend-dev6 <kodeend.dev6@gmail.com>
2023-11-15 10:05:54 +0600

    modified craete match

--- controllers/soccer.controller.js
+++ controllers/soccer.controller.js
@@ -90,7 +90,10 @@ const createNewMatch = async (req, res) => {
     });
 
     console.log('Record added successfully:', response.data);
-    res.status(201).send('Record added successfully to Airtable.');
+    res.status(201).json({
+      success: true,
+      message: "Successfully added.."
+    });
   } catch (error) {
     console.error('Error adding record:', error);
     res.status(500).send('Error adding record to Airtable.');
