79e58cb59a14269ae976797e4240cbc4a02bdb72
Horizon-dev4 <horizonsolutions.dev4@gmail.com>
2023-11-22 15:53:04 +0600

    soccer time issue resolved

--- controllers/soccer.controller.js
+++ controllers/soccer.controller.js
@@ -1,17 +1,20 @@
 const config = require("../config/config");
 const soccerTable = config.db.soccerTableUrl;
 const apiKey = config.key.apiKey;
-const axios = require('axios');
-const moment = require('moment');
-const moment2 = require('moment-timezone');
-const { convertedFromDB, convertedToDB } = require("../utils/dateAndTimeConverter");
-
+const axios = require("axios");
+const moment = require("moment");
+const moment2 = require("moment-timezone");
+const {
+  convertedFromDB,
+  convertedToDB,
+} = require("../utils/dateAndTimeConverter");
+const catchAsync = require("../utils/errors/catchAsync");
+const sendResponse = require("../utils/sendResponse");
 
 const getAllSoccerMatches = async (req, res) => {
   try {
-
     const headers = {
-      'Authorization': `Bearer ${apiKey}`,
+      Authorization: `Bearer ${apiKey}`,
     };
 
     const response = await axios.get(soccerTable, { headers });
@@ -21,100 +24,113 @@ const getAllSoccerMatches = async (req, res) => {
     res.json(data);
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: 'Failed to fetch data from Airtable' });
-  }
-}
-
-const getAllSoccerMatchesByDate = async (req, res) => {
-  const { value, time, timeZone, filter } = req.query;
-
-  if (filter === 'finished') {
-    try {
-      
-      const selectedDay = await convertedToDB(value, time, timeZone);
-      const threeDaysAgo = moment(selectedDay).subtract(3, 'days').format('YYYY-MM-DD');
-
-      const field = 'Date';
-
-      const url = `${soccerTable}?filterByFormula=AND({${field}}<='${selectedDay}', {${field}}>'${threeDaysAgo}', {upload}=1)&sort%5B0%5D%5Bfield%5D=${field}&sort%5B0%5D%5Bdirection%5D=desc`;
-
-      const headers = {
-        Authorization: `Bearer ${apiKey}`,
-      };
-
-      const response = await axios.get(url, { headers });
-      const allData = response.data.records;
-
-      const filterData = allData.filter(data => data.fields.Results);
-
-      const convertedDatas = await convertedFromDB(filterData, timeZone);
-
-      res.json(convertedDatas);
-    } catch (error) {
-      console.error('Previous match get error', error);
-      res.status(500).json({ error: 'An error occurred while fetching data.' });
-    }
-
-  }
-  else if (filter === 'schedule') {
-    try {
-      
-      const selectedDay = await convertedToDB(value, time, timeZone);
-      const threeDaysNext = moment(selectedDay).add(3, 'days').format('YYYY-MM-DD');
-
-      const field = 'Date';
-
-      const url = `${soccerTable}?filterByFormula=AND({${field}}>='${selectedDay}', {${field}}<='${threeDaysNext}', {upload}=1)&sort%5B0%5D%5Bfield%5D=${field}&sort%5B0%5D%5Bdirection%5D=asc`;
-
-      const headers = {
-        Authorization: `Bearer ${apiKey}`,
-      };
-
-      const response = await axios.get(url, { headers });
-      const allData = response.data.records;
-      const convertedDatas = await convertedFromDB(allData, timeZone);
-      res.json(convertedDatas);
-    } catch (error) {
-      console.error('Previous match get error', error);
-      res.status(500).json({ error: 'An error occurred while fetching data.' });
-    }
-  }
-  else {
-    try {
-      const field = "Date";
-
-      if (!field || !value) {
-        return res.status(400).json({ error: 'Both field and value parameters are required.' });
-      }
-
-      const convertedDate = await convertedToDB(value, time, timeZone);
-
-
-      const url = `${soccerTable}?filterByFormula=AND({${field}}='${convertedDate}', {upload}=1)`;
-      const headers = {
-        Authorization: `Bearer ${apiKey}`,
-      };
-
-      const response = await axios.get(url, { headers });
-      const allData = response.data.records;
-
-      const convertedDatas = await convertedFromDB(allData, timeZone);
-
-      res.json(convertedDatas);
-    } catch (error) {
-      console.error('Error fetching data from Airtable:', error);
-      res.status(500).json({ error: 'An error occurred while fetching data.' });
-    }
+    res.status(500).json({ error: "Failed to fetch data from Airtable" });
   }
 };
 
+// Get All Soccer Matches including finished and next matches
+const getAllSoccerMatchesByDate = catchAsync(async (req, res) => {
+  const { value, time, timeZone, filter } = req.query;
+
+  // Filtering Finished Matches
+  if (filter === "finished") {
+    const selectedDay = await convertedToDB(value, time, timeZone);
+    const threeDaysAgo = moment(selectedDay)
+      .subtract(3, "days")
+      .format("YYYY-MM-DD");
+
+    const field = "Date";
+
+    const url = `${soccerTable}?filterByFormula=AND({${field}}<='${selectedDay}', {${field}}>'${threeDaysAgo}', {upload}=1)&sort%5B0%5D%5Bfield%5D=${field}&sort%5B0%5D%5Bdirection%5D=desc`;
+
+    const headers = {
+      Authorization: `Bearer ${apiKey}`,
+    };
+
+    const response = await axios.get(url, { headers });
+    const allData = response.data.records;
+
+    const filterData = allData.filter((data) => data.fields.Results);
+
+    const convertedDatas = await convertedFromDB(filterData, timeZone, value);
+
+    sendResponse(res, {
+      statusCode: 200,
+      success: true,
+      message: "Soccer Match retrieved successfully",
+      data: convertedDatas,
+    });
+
+    // Filtering Next Matches
+  } else if (filter === "schedule") {
+    const selectedDay = await convertedToDB(value, time, timeZone);
+    const threeDaysNext = moment(selectedDay)
+      .add(3, "days")
+      .format("YYYY-MM-DD");
+
+    const field = "Date";
+
+    const url = `${soccerTable}?filterByFormula=AND({${field}}>='${selectedDay}', {${field}}<='${threeDaysNext}', {upload}=1)&sort%5B0%5D%5Bfield%5D=${field}&sort%5B0%5D%5Bdirection%5D=asc`;
+
+    const headers = {
+      Authorization: `Bearer ${apiKey}`,
+    };
+
+    const response = await axios.get(url, { headers });
+    const allData = response.data.records;
+    const convertedDatas = await convertedFromDB(allData, timeZone, value);
+
+    sendResponse(res, {
+      statusCode: 200,
+      success: true,
+      message: "Soccer Match retrieved successfully",
+      data: convertedDatas,
+    });
+
+    // Filtering All Matches
+  } else {
+    const field = "Date";
+
+    if (!field || !value) {
+      return res
+        .status(400)
+        .json({ error: "Both field and value parameters are required." });
+    }
+
+    const convertedDate = await convertedToDB(value, time, timeZone);
+    const startDate = moment(convertedDate)
+      .subtract(1, "days")
+      .format("YYYY-MM-DD");
+    const endDate = moment(convertedDate).add(1, "days").format("YYYY-MM-DD");
+
+    const url = `${soccerTable}?filterByFormula=AND(OR({${field}}='${startDate}', {${field}}='${convertedDate}', {${field}}='${endDate}'), {upload}=1)`;
+
+    const headers = {
+      Authorization: `Bearer ${apiKey}`,
+    };
+
+    const response = await axios.get(url, { headers });
+    const allData = response.data.records;
+
+    const convertedDatas = await convertedFromDB(allData, timeZone, value);
+
+    sendResponse(res, {
+      statusCode: 200,
+      success: true,
+      message: "Soccer Match retrieved successfully",
+      data: convertedDatas,
+    });
+  }
+});
 
 const getAllFinishedSoccerMatches = async (req, res) => {
   try {
     const selectedDay = req.query.selectedDay;
-    const threeDaysAgo = moment(selectedDay).subtract(3, 'days').format('YYYY-MM-DD');
+    const threeDaysAgo = moment(selectedDay)
+      .subtract(3, "days")
+      .format("YYYY-MM-DD");
 
-    const field = 'Date';
+    const field = "Date";
 
     const url = `${soccerTable}?filterByFormula=AND({${field}}<='${selectedDay}', {${field}}>'${threeDaysAgo}')&sort%5B0%5D%5Bfield%5D=${field}&sort%5B0%5D%5Bdirection%5D=desc`;
 
@@ -129,17 +145,19 @@ const getAllFinishedSoccerMatches = async (req, res) => {
 
     res.json(filteredData);
   } catch (error) {
-    console.error('Previous match get error', error);
-    res.status(500).json({ error: 'An error occurred while fetching data.' });
+    console.error("Previous match get error", error);
+    res.status(500).json({ error: "An error occurred while fetching data." });
   }
 };
 
 const getAllNextSoccerMatches = async (req, res) => {
   try {
     const selectedDay = req.query.selectedDay;
-    const threeDaysNext = moment(selectedDay).add(3, 'days').format('YYYY-MM-DD');
+    const threeDaysNext = moment(selectedDay)
+      .add(3, "days")
+      .format("YYYY-MM-DD");
 
-    const field = 'Date';
+    const field = "Date";
 
     const url = `${soccerTable}?filterByFormula=AND({${field}}>='${selectedDay}', {${field}}<='${threeDaysNext}')&sort%5B0%5D%5Bfield%5D=${field}&sort%5B0%5D%5Bdirection%5D=asc`;
 
@@ -154,59 +172,54 @@ const getAllNextSoccerMatches = async (req, res) => {
 
     res.json(filteredData);
   } catch (error) {
-    console.error('Previous match get error', error);
-    res.status(500).json({ error: 'An error occurred while fetching data.' });
+    console.error("Previous match get error", error);
+    res.status(500).json({ error: "An error occurred while fetching data." });
   }
 };
 
+// Get Single Soccer Match
+const getSingleSoccerMatch = catchAsync(async (req, res) => {
+  const matchID = req.params.matchID;
 
+  const url = `${soccerTable}/${matchID}`;
+  const headers = {
+    Authorization: `Bearer ${apiKey}`,
+  };
 
-const getSingleSoccerMatch = async (req, res) => {
-  try {
+  const response = await axios.get(url, { headers });
+  const data = response.data;
 
-    const matchID = req.params.matchID;
-
-    const url = `${soccerTable}/${matchID}`;
-    const headers = {
-      Authorization: `Bearer ${apiKey}`,
-    };
-
-    const response = await axios.get(url, { headers });
-    const data = response.data;
-
-
-    res.json(data);
-  } catch (error) {
-    console.error('Error fetching data from Airtable:', error);
-    res.status(500).json({ error: 'An error occurred while fetching data.' });
-  }
-};
-
+  sendResponse(res, {
+    statusCode: 200,
+    success: true,
+    message: "Soccer Match retrieved successfully",
+    data,
+  });
+});
 
 const createNewMatch = async (req, res) => {
   const { fields } = req.body;
 
-  const data = { fields }
+  const data = { fields };
 
   try {
     const response = await axios.post(soccerTable, data, {
       headers: {
         Authorization: `Bearer ${apiKey}`,
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
     });
 
-    console.log('Record added successfully:', response.data);
+    console.log("Record added successfully:", response.data);
     res.status(201).json({
       success: true,
-      message: "Successfully added.."
+      message: "Successfully added..",
     });
   } catch (error) {
-    console.error('Error adding record:', error);
-    res.status(500).send('Error adding record to Airtable.');
+    console.error("Error adding record:", error);
+    res.status(500).send("Error adding record to Airtable.");
   }
 };
-
 
 const deleteOneMatch = async (req, res) => {
   const { recordId } = req.params;
@@ -220,20 +233,19 @@ const deleteOneMatch = async (req, res) => {
     const response = await axios.delete(airtableURL, { headers });
 
     if (response.status === 200) {
-      res.status(204).json({ message: 'Record deleted successfully' });
+      res.status(204).json({ message: "Record deleted successfully" });
     } else {
       res.status(response.status).json(response.data);
     }
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: 'Error deleting the record' });
+    res.status(500).json({ message: "Error deleting the record" });
   }
 };
 
 const updateOneMatch = async (req, res) => {
   const { recordId } = req.params;
   const { fields } = req.body;
-
 
   try {
     const airtableURL = `${soccerTable}/${recordId}`;
@@ -246,16 +258,23 @@ const updateOneMatch = async (req, res) => {
     const response = await axios.patch(airtableURL, data, { headers });
 
     if (response.status === 200) {
-      res.status(200).json({ message: 'Record updated successfully' });
+      res.status(200).json({ message: "Record updated successfully" });
     } else {
       res.status(response.status).json(response.data);
     }
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: 'Error updating the record' });
+    res.status(500).json({ message: "Error updating the record" });
   }
 };
 
-
-
-module.exports = { getAllSoccerMatches, getSingleSoccerMatch, getAllSoccerMatchesByDate, createNewMatch, deleteOneMatch, updateOneMatch, getAllFinishedSoccerMatches, getAllNextSoccerMatches }
+module.exports = {
+  getAllSoccerMatches,
+  getSingleSoccerMatch,
+  getAllSoccerMatchesByDate,
+  createNewMatch,
+  deleteOneMatch,
+  updateOneMatch,
+  getAllFinishedSoccerMatches,
+  getAllNextSoccerMatches,
+};
--- utils/dateAndTimeConverter.js
+++ utils/dateAndTimeConverter.js
@@ -1,73 +1,104 @@
-const moment2 = require('moment-timezone');
-
+const moment2 = require("moment-timezone");
 
 const convertedToDB = async (value, time, timeZone) => {
   const currentDate = `${value} ${time}`;
-  const convertedDateTime = moment2.tz(currentDate, 'YYYY-MM-DD HH:mm', `${timeZone}`).tz('Europe/Stockholm');
-  const convertedDate = convertedDateTime.format('YYYY-MM-DD');
+  const convertedDateTime = moment2
+    .tz(currentDate, "YYYY-MM-DD HH:mm", `${timeZone}`)
+    .tz("Europe/Stockholm");
+  const convertedDate = convertedDateTime.format("YYYY-MM-DD");
   return convertedDate;
-}
-
+};
 
 const convertedToDBValorant = async (value, time, timeZone) => {
   const currentDate = `${value} ${time}`;
-  const convertedDateTime = moment2.tz(currentDate, 'YYYY-MM-DD HH:mm', `${timeZone}`).tz('Asia/Dhaka');
-  const convertedDate = convertedDateTime.format('YYYY-MM-DD');
+  const convertedDateTime = moment2
+    .tz(currentDate, "YYYY-MM-DD HH:mm", `${timeZone}`)
+    .tz("Asia/Dhaka");
+  const convertedDate = convertedDateTime.format("YYYY-MM-DD");
   return convertedDate;
-}
-
-
+};
 
 const convertedFromDBValorant = async (allData, desiredTimeZone) => {
-  const item = await allData.map(item => {
+  const item = await allData.map((item) => {
     // Convert date and time
     if (item.fields.Date && item.fields.Time) {
       const bdDateTime = `${item.fields.Date} ${item.fields.Time}`;
-      const convertedDateTime = moment2.tz(bdDateTime, 'YYYY-MM-DD h:mm A', 'Asia/Dhaka').tz(desiredTimeZone);
+      const convertedDateTime = moment2
+        .tz(bdDateTime, "YYYY-MM-DD h:mm A", "Asia/Dhaka")
+        .tz(desiredTimeZone);
 
-      item.fields.Date = convertedDateTime.format('YYYY-MM-DD'); // Update the date field
-      item.fields.Time = convertedDateTime.format('HH:mm'); // Update the time field
+      item.fields.Date = convertedDateTime.format("YYYY-MM-DD"); // Update the date field
+      item.fields.Time = convertedDateTime.format("HH:mm"); // Update the time field
     }
 
     return item;
   });
 
-  return item
+  return item;
 };
 
-const convertedFromDB = async (allData, desiredTimeZone) => {
-  const item = await allData.map(item => {
-    // Convert date and time
+const convertedFromDB = async (allData, desiredTimeZone, desiredDate) => {
+  // const items = await allData.map(item => {
+  //   if (item.fields.Date && item.fields.Time) {
+  //     const swedenDateTime = `${item.fields.Date} ${item.fields.Time}`;
+  //     const convertedDateTime = moment2.tz(swedenDateTime, 'YYYY-MM-DD HH:mm', 'Europe/Stockholm').tz(desiredTimeZone);
+
+  //     item.fields.Date = convertedDateTime.format('YYYY-MM-DD'); // Update the date field
+  //     item.fields.Time = convertedDateTime.format('HH:mm'); // Update the time field
+  //   }
+
+  //   return item;
+  // });
+
+  // return items
+
+  const convertedData = [];
+  for (const item of allData) {
     if (item.fields.Date && item.fields.Time) {
       const swedenDateTime = `${item.fields.Date} ${item.fields.Time}`;
-      const convertedDateTime = moment2.tz(swedenDateTime, 'YYYY-MM-DD h:mm A', 'Europe/Stockholm').tz(desiredTimeZone);
+      const convertedDateTime = moment2
+        .tz(swedenDateTime, "YYYY-MM-DD HH:mm", "Europe/Stockholm")
+        .tz(desiredTimeZone);
 
-      item.fields.Date = convertedDateTime.format('YYYY-MM-DD'); // Update the date field
-      item.fields.Time = convertedDateTime.format('HH:mm'); // Update the time field
+      
+      if (convertedDateTime.format("YYYY-MM-DD") !== desiredDate) {
+        continue;
+      }
+
+      item.fields.Date = convertedDateTime.format("YYYY-MM-DD"); // Update the date field
+      item.fields.Time = convertedDateTime.format("HH:mm"); // Update the time field
     }
 
-    return item;
-  });
-
-  return item
+    convertedData.push(item);
+  }
+  return convertedData;
 };
 
 const convertedFromDBCSGO = async (allData, desiredTimeZone) => {
-  const item = await allData.map(item => {
+  const item = await allData.map((item) => {
     if (item.fields.Date && item.fields.Time) {
       const dateComponents = item.fields.Date.split("-");
-      const date = dateComponents[2] + "-" + dateComponents[1] + "-" + dateComponents[0];
+      const date =
+        dateComponents[2] + "-" + dateComponents[1] + "-" + dateComponents[0];
 
       const swedenDateTime = `${date} ${item.fields.Time}`;
-      const convertedDateTime = moment2.tz(swedenDateTime, 'YYYY-MM-DD h:mm A', 'Europe/Stockholm').tz(desiredTimeZone);
+      const convertedDateTime = moment2
+        .tz(swedenDateTime, "YYYY-MM-DD h:mm A", "Europe/Stockholm")
+        .tz(desiredTimeZone);
 
-      item.fields.Date = convertedDateTime.format('YYYY-MM-DD');
-      item.fields.Time = convertedDateTime.format('HH:mm');
+      item.fields.Date = convertedDateTime.format("YYYY-MM-DD");
+      item.fields.Time = convertedDateTime.format("HH:mm");
     }
     return item;
   });
 
-  return item
+  return item;
 };
 
-module.exports = { convertedFromDB, convertedToDB, convertedFromDBCSGO, convertedToDBValorant, convertedFromDBValorant }
+module.exports = {
+  convertedFromDB,
+  convertedToDB,
+  convertedFromDBCSGO,
+  convertedToDBValorant,
+  convertedFromDBValorant,
+};
