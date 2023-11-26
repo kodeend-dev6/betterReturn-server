f6b012e03e2ecd52e2c7d1c8e6b48e101396e744
Horizon-dev4 <horizonsolutions.dev4@gmail.com>
2023-11-26 15:57:23 +0600

    middleware added to stripe webhook

--- routes/payment.route.js
+++ routes/payment.route.js
@@ -12,6 +12,10 @@ router.post("/subscription", createSubscription);
 router.post("/cancel-subscription", cancelSubscription);
 
 // Stripe Webhook
-router.post("/webhook", stripeWebhook);
+router.post(
+  "/webhook",
+  express.raw({ type: "application/json" }),
+  stripeWebhook
+);
 
 module.exports = router;
