3fdcd4794a751b3ce6bbf98662738e3038bfb5ba
kodeend-dev6 <kodeend.dev6@gmail.com>
2023-12-18 11:11:36 +0600

    updeted roi

--- controllers/roi.controller.js
+++ controllers/roi.controller.js
@@ -1,4 +1,3 @@
-const axios = require("axios");
 const config = require("../config/config");
 const catchAsync = require("../utils/errors/catchAsync");
 const sendResponse = require("../utils/sendResponse");
@@ -9,49 +8,40 @@ const getRoi = catchAsync(async (req, res) => {
     const { startDate, endDate, initialBalance, percent } = req.query;
 
     try {
-
         const startedDate = new Date(startDate);
-
         let currentDate = new Date(endDate);
-
         let finalBalance = initialBalance;
 
         while (currentDate <= startedDate) {
-
-
-
             const result = await fetcher.get(soccerTable, {
                 params: {
-                    maxRecords: 200,
+                    fields: ["Results", "PredictedOdds"],
                     filterByFormula: `AND({Date} = '${currentDate.toISOString().slice(0, 10)}', {upload} = 1, NOT({MatchResults} = ''))`,
                     sort: [{ field: "Date", direction: "asc" }]
                 },
             });
 
-
             const records = result?.data?.records;
-
             const percentInvestment = percent / 100;
 
             if (records.length > 0) {
-                let winOdds = 0;
-                let winM = 0;
-
-                for (let i = 0; i < records?.length; i++) {
-                    if (records[i].fields.Results === 'TRUE') {
-                        winOdds += records[i]?.fields?.PredictedOdds
-                        winM++;
+                const winOdds = records.reduce((sum, record) => {
+                    if (record.fields.Results === 'TRUE') {
+                        return sum + (record.fields.PredictedOdds || 0);
                     }
+                    return sum;
+                }, 0);
+
+                const winM = records.filter(record => record.fields.Results === 'TRUE').length;
+
+                if (winM > 0) {
+                    const avgOdds = winOdds / winM;
+                    finalBalance = (finalBalance - (finalBalance * percentInvestment)) + (avgOdds * (winM / records.length) * finalBalance * percentInvestment);
                 }
-
-                let avgOdds = winOdds / winM;
-                let winPerc = winM / records?.length;
-
-                finalBalance = (finalBalance - (finalBalance * percentInvestment)) + (avgOdds * winPerc * finalBalance * percentInvestment);
             }
+
             currentDate.setDate(currentDate.getDate() + 1);
         }
-
 
         sendResponse(res, {
             statusCode: 200,
@@ -70,3 +60,4 @@ const getRoi = catchAsync(async (req, res) => {
 });
 
 module.exports = { getRoi };
+
