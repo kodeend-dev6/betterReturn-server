cfab802dc6637f223f197a30531bd326548924bf
kodeend-dev6 <kodeend.dev6@gmail.com>
2024-01-01 10:13:53 +0600

    updated combo api

--- controllers/combo.controller.js
+++ controllers/combo.controller.js
@@ -2,6 +2,7 @@ const catchAsync = require("../utils/errors/catchAsync");
 const fetcher = require("../utils/fetcher/airTableFetcher");
 const sendResponse = require("../utils/sendResponse");
 const config = require("../config/config");
+const ApiError = require("../utils/errors/ApiError");
 
 const getAllCombo = catchAsync(async (req, res) => {
   const date = req.query.date;
@@ -27,34 +28,34 @@ const getAllCombo = catchAsync(async (req, res) => {
         return record?.fields[GamenameIndex] === "soccer"
           ? "MatchID"
           : record?.fields[GamenameIndex] === "csgo"
-          ? "Match-id"
-          : record?.fields[GamenameIndex] === "valorant"
-          ? "Match-id"
-          : null;
+            ? "Match-id"
+            : record?.fields[GamenameIndex] === "valorant"
+              ? "Match-id"
+              : null;
       };
 
       const match1 = record?.fields?.MatchID1
         ? {
-            matchId: record?.fields?.MatchID1,
-            tableUrl: config.db[`${record?.fields?.Gamename1}TableUrl`],
-            columnName: gameColumnName("Gamename1"),
-          }
+          matchId: record?.fields?.MatchID1,
+          tableUrl: config.db[`${record?.fields?.Gamename1}TableUrl`],
+          columnName: gameColumnName("Gamename1"),
+        }
         : null;
 
       const match2 = record?.fields?.MatchID2
         ? {
-            matchId: record?.fields?.MatchID2,
-            tableUrl: config.db[`${record?.fields?.Gamename2}TableUrl`],
-            columnName: gameColumnName("Gamename2"),
-          }
+          matchId: record?.fields?.MatchID2,
+          tableUrl: config.db[`${record?.fields?.Gamename2}TableUrl`],
+          columnName: gameColumnName("Gamename2"),
+        }
         : null;
 
       const match3 = record?.fields?.MatchID3
         ? {
-            matchId: record?.fields?.MatchID3,
-            tableUrl: config.db[`${record?.fields?.Gamename3}TableUrl`],
-            columnName: gameColumnName("Gamename3"),
-          }
+          matchId: record?.fields?.MatchID3,
+          tableUrl: config.db[`${record?.fields?.Gamename3}TableUrl`],
+          columnName: gameColumnName("Gamename3"),
+        }
         : null;
 
       if (match1) {
@@ -135,34 +136,34 @@ const getAllCombo = catchAsync(async (req, res) => {
         return record?.fields[GamenameIndex] === "soccer"
           ? "MatchID"
           : record?.fields[GamenameIndex] === "csgo"
-          ? "Match-id"
-          : record?.fields[GamenameIndex] === "valorant"
-          ? "Match-id"
-          : null;
+            ? "Match-id"
+            : record?.fields[GamenameIndex] === "valorant"
+              ? "Match-id"
+              : null;
       };
 
       const match1 = record?.fields?.MatchID1
         ? {
-            matchId: record?.fields?.MatchID1,
-            tableUrl: config.db[`${record?.fields?.Gamename1}TableUrl`],
-            columnName: gameColumnName("Gamename1"),
-          }
+          matchId: record?.fields?.MatchID1,
+          tableUrl: config.db[`${record?.fields?.Gamename1}TableUrl`],
+          columnName: gameColumnName("Gamename1"),
+        }
         : null;
 
       const match2 = record?.fields?.MatchID2
         ? {
-            matchId: record?.fields?.MatchID2,
-            tableUrl: config.db[`${record?.fields?.Gamename2}TableUrl`],
-            columnName: gameColumnName("Gamename2"),
-          }
+          matchId: record?.fields?.MatchID2,
+          tableUrl: config.db[`${record?.fields?.Gamename2}TableUrl`],
+          columnName: gameColumnName("Gamename2"),
+        }
         : null;
 
       const match3 = record?.fields?.MatchID3
         ? {
-            matchId: record?.fields?.MatchID3,
-            tableUrl: config.db[`${record?.fields?.Gamename3}TableUrl`],
-            columnName: gameColumnName("Gamename3"),
-          }
+          matchId: record?.fields?.MatchID3,
+          tableUrl: config.db[`${record?.fields?.Gamename3}TableUrl`],
+          columnName: gameColumnName("Gamename3"),
+        }
         : null;
 
       if (match1) {
@@ -229,110 +230,202 @@ const getAllCombo = catchAsync(async (req, res) => {
 });
 
 // Get All combo v2
+// const getAllComboV2 = catchAsync(async (req, res) => {
+//   const date = req.query.date;
+//   let data = [];
+
+//   for (let i = 0; i < 5; i++) {
+//     const newCombo = [];
+//     const soccerResult = await fetcher.get(config.db.soccerTableUrl, {
+//       params: {
+//         fields: [
+//           "MatchID",
+//           "Date",
+//           "Time",
+//           "HomeTeam",
+//           "AwayTeam",
+//           "HomeGoals",
+//           "AwayGoals",
+//           "HomeOdds",
+//           "AwayOdds",
+//           "DrawOdds",
+//           "PredictedOdds",
+//           "MatchResults",
+//           "Results",
+//           "Prediction",
+//           "T1Logo",
+//           "T2Logo",
+//           "LeagueName",
+//           "Venue",
+//         ],
+//         filterByFormula: `AND({Date} = '${date}', {Combo} = ${i + 1})`,
+//       },
+//     });
+
+//     const csgoResult = await fetcher.get(config.db.csgoTableUrl, {
+//       params: {
+//         filterByFormula: `AND({Date} = '${date}', {Combo} = ${i + 1})`,
+//       },
+//     });
+
+//     const valorantResult = await fetcher.get(config.db.valorantTableUrl, {
+//       params: {
+//         filterByFormula: `AND({Date} = '${date}', {Combo} = ${i + 1})`,
+//       },
+//     });
+
+//     const soccerData = soccerResult?.data?.records;
+//     const csgoData = csgoResult?.data?.records;
+//     const valorantData = valorantResult?.data?.records;
+
+//     //  use a loop to get the data
+//     if (soccerData?.length) {
+//       for (let s = 0; s < soccerData?.length; s++) {
+//         const soccerNewData = {
+//           name: "soccer",
+//           data: {
+//             recordId: soccerData[s]?.id,
+//             ...soccerData[s]?.fields,
+//           },
+//         };
+
+//         newCombo.push(soccerNewData);
+//       }
+//     }
+
+//     if (csgoData?.length) {
+//       for (let c = 0; c < csgoData?.length; c++) {
+//         const csgoNewData = {
+//           name: "csgo",
+//           data: {
+//             recordId: csgoData[c]?.id,
+//             ...csgoData[c]?.fields,
+//           },
+//         };
+
+//         newCombo.push(csgoNewData);
+//       }
+//     }
+
+//     if (valorantData?.length) {
+//       for (let v = 0; v < valorantData?.length; v++) {
+//         const valorantNewData = {
+//           name: "valorant",
+//           data: {
+//             recordId: valorantData[v]?.id,
+//             ...valorantData[v]?.fields,
+//           },
+//         };
+
+//         newCombo.push(valorantNewData);
+//       }
+//     }
+
+//     newCombo?.length ? data.push(newCombo) : null;
+//   }
+
+//   sendResponse(res, {
+//     success: true,
+//     statusCode: 200,
+//     message: "Retrieved all combo successfully!",
+//     data,
+//     meta: {
+//       total: data.length,
+//     },
+//   });
+// });
+
+
 const getAllComboV2 = catchAsync(async (req, res) => {
-  const date = req.query.date;
-  let data = [];
+  try {
+    const date = req.query.date;
+    const data = [];
 
-  for (let i = 0; i < 3; i++) {
-    const newCombo = [];
-    const soccerResult = await fetcher.get(config.db.soccerTableUrl, {
-      params: {
-        fields: [
-          "MatchID",
-          "Date",
-          "Time",
-          "HomeTeam",
-          "AwayTeam",
-          "HomeGoals",
-          "AwayGoals",
-          "HomeOdds",
-          "AwayOdds",
-          "DrawOdds",
-          "PredictedOdds",
-          "MatchResults",
-          "Results",
-          "Prediction",
-          "T1Logo",
-          "T2Logo",
-          "LeagueName",
-          "Venue",
-        ],
-        filterByFormula: `AND({Date} = '${date}', {Combo} = ${i + 1})`,
-      },
-    });
+    const fetchPromises = [];
 
-    // const csgoResult = await fetcher.get(config.db.csgoTableUrl, {
-    //   params: {
-    //     filterByFormula: `AND({Date} = '${date}', {Combo} = ${i + 1})`,
-    //   },
-    // });
-
-    // const valorantResult = await fetcher.get(config.db.valorantTableUrl, {
-    //   params: {
-    //     filterByFormula: `AND({Date} = '${date}', {Combo} = ${i + 1})`,
-    //   },
-    // });
-
-    const soccerData = soccerResult?.data?.records;
-    // const csgoData = csgoResult?.data?.records;
-    // const valorantData = valorantResult?.data?.records;
-
-    //  use a loop to get the data
-    if (soccerData?.length) {
-      for (let s = 0; s < soccerData?.length; s++) {
-        const soccerNewData = {
-          name: "soccer",
-          data: {
-            recordId: soccerData[s]?.id,
-            ...soccerData[s]?.fields,
-          },
-        };
-
-        newCombo.push(soccerNewData);
-      }
+    for (let i = 0; i < 5; i++) {
+      fetchPromises.push(
+        Promise.all([
+          fetcher.get(config.db.soccerTableUrl, {
+            params: {
+              fields: [
+                "MatchID",
+                "Date",
+                "Time",
+                "HomeTeam",
+                "AwayTeam",
+                "HomeGoals",
+                "AwayGoals",
+                "HomeOdds",
+                "AwayOdds",
+                "DrawOdds",
+                "PredictedOdds",
+                "MatchResults",
+                "Results",
+                "Prediction",
+                "T1Logo",
+                "T2Logo",
+                "LeagueName",
+                "Venue",
+              ],
+              filterByFormula: `AND({Date} = '${date}', {Combo} = ${i + 1})`,
+            },
+          }),
+          fetcher.get(config.db.csgoTableUrl, {
+            params: {
+              filterByFormula: `AND({Date} = '${date}', {Combo} = ${i + 1})`,
+            },
+          }),
+          fetcher.get(config.db.valorantTableUrl, {
+            params: {
+              filterByFormula: `AND({Date} = '${date}', {Combo} = ${i + 1})`,
+            },
+          }),
+        ])
+      );
     }
 
-    // if (csgoData?.length) {
-    //   for (let c = 0; c < csgoData?.length; c++) {
-    //     const csgoNewData = {
-    //       name: "csgo",
-    //       data: {
-    //         recordId: csgoData[c]?.id,
-    //         ...csgoData[c]?.fields,
-    //       },
-    //     };
+    const results = await Promise.all(fetchPromises);
 
-    //     newCombo.push(csgoNewData);
-    //   }
-    // }
+    results.forEach(([soccerResult, csgoResult, valorantResult]) => {
+      const comboData = [];
 
-    // if (valorantData?.length) {
-    //   for (let v = 0; v < valorantData?.length; v++) {
-    //     const valorantNewData = {
-    //       name: "valorant",
-    //       data: {
-    //         recordId: valorantData[v]?.id,
-    //         ...valorantData[v]?.fields,
-    //       },
-    //     };
+      [soccerResult?.data?.records, csgoResult?.data?.records, valorantResult?.data?.records].forEach(
+        (dataArr, index) => {
+          if (dataArr?.length) {
+            const sport = index === 0 ? 'soccer' : index === 1 ? 'csgo' : 'valorant';
+            const newData = dataArr.map(item => ({
+              name: sport,
+              data: {
+                recordId: item?.id,
+                ...item?.fields,
+              },
+            }));
+            comboData.push(newData);
+          }
+        }
+      );
 
-    //     newCombo.push(valorantNewData);
-    //   }
-    // }
+      if (comboData.length > 0) {
+        data.push(comboData);
+      }
+    });
 
-    newCombo?.length ? data.push(newCombo) : null;
+    sendResponse(res, {
+      success: true,
+      statusCode: 200,
+      message: "Retrieved all combo successfully!",
+      data,
+      meta: {
+        total: data.length,
+      },
+    });
+  } catch (error) {
+    throw new Error("Combo data fetching error");
   }
-
-  sendResponse(res, {
-    success: true,
-    statusCode: 200,
-    message: "Retrieved all combo successfully!",
-    data,
-    meta: {
-      total: data.length,
-    },
-  });
 });
+
+
 
 module.exports = {
   getAllCombo,
